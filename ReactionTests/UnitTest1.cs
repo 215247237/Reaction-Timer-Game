1c285e65829a141f81516ebc1261c791a9ca2e57
Jacqui <jacqui@Michaels-iMac-2.local>
2025-09-25 15:08:36 +1000

    Add ReactionTests project

---
+++ ReactionTests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using SimpleReactionMachine;
+
+namespace ReactionTests
+{
+    public class ReactionControllerTests
+    {
+        private EnhancedReactionController controller;
+        private DummyGui gui;
+        private DummyRandom rng;
+
+        [SetUp]
+        public void Setup()
+        {
+            gui = new DummyGui();
+            rng = new DummyRandom();
+            controller = new EnhancedReactionController();
+            controller.Connect(gui, rng);
+            controller.Init();
+        }
+
+        [Test]
+        public void CoinInserted_ShowsPressGoMessage()
+        {
+            controller.CoinInserted();
+            Assert.That(gui.LastDisplay, Is.EqualTo("Press GO!"));
+        }
+
+        [Test]
+        public void ReadyState_TimesOutAfter10Seconds()
+        {
+            controller.CoinInserted(); // go to ReadyState
+            for (int i = 0; i < 1000; i++) controller.Tick();
+            Assert.That(gui.LastDisplay, Is.EqualTo("Insert coin"));
+        }
+
+        [Test]
+        public void GoStopPressed_TransitionsToDelayState()
+        {
+            controller.CoinInserted(); // ReadyState
+            controller.GoStopPressed();
+            Assert.That(gui.LastDisplay, Is.EqualTo("Wait..."));
+        }
+
+
+        private class DummyGui : IGui
+        {
+            public string LastDisplay { get; private set; } = "";
+            public void Connect(IController controller) { }
+            public void Init() { }
+            public void SetDisplay(string s) { LastDisplay = s; }
+        }
+
+        private class DummyRandom : IRandom
+        {
+            private int value = 100;
+            public void SetRandom(int v) => value = v;
+            public int GetRandom(int from, int to) => value;
+        }
+    }
+}
